5d194628afe38b5feca241640b401895914e35a9
Muchammad Ibnu <muchammadibnu@gmail.com>
2020-03-25 00:28:00 +0700

    Add comment

--- tucil3.py
+++ tucil3.py
@@ -1,5 +1,13 @@
 import time
 import copy
+''' Tucil 3 Strategi Algoritma
+    Nama    : Muchammad Ibnu Sidqi
+    Nim     : 13518072
+    Kelas   : K-3 
+'''
+''' Fungsi untuk menghitung fungsi g(x) dari suatu input Puzzle(Ubin), 
+    untuk f(x) didapatkan dari level simpul diluar fungsi 
+'''
 def costPuzzle(Puzzle):
     cost=0
     for i in range(0,4):
@@ -12,6 +20,10 @@ def costPuzzle(Puzzle):
                     if(Puzzle[i][j]!=16):
                         cost= cost + 1
     return cost
+''' Fungsi untuk menghitung apakah suatu 15-Puzzle Solved atau Unsolved 
+    dengan menghitung nilai kurang pada masing masing indeks serta faktor X. 
+    Fungsi akan mengembalikkan true jika persoalan dapat diselesaikan 
+'''
 def kurangPuzzle(Puzzle):
     kurang=0
     x=0
@@ -51,6 +63,8 @@ def kurangPuzzle(Puzzle):
     else:
         print("Persoalan DAPAT diselesaikan")
         return True
+''' Prosedur untuk menampilkan ke layar 15-Puzzle 
+''' 
 def printPuzzle(Puzzle):
     print("----------------------------------------")
     for i in range(0,4):
@@ -64,6 +78,9 @@ def printPuzzle(Puzzle):
                 print("    ",end='')
         print()
     print("----------------------------------------")
+''' Fungsi untuk mengambil simpul dari Queue yang memiliki cost terkecil. 
+    Mengembalikkan simpul dari antrian Queue yang memiliki nilai terkecil. 
+'''
 def popQueue(Queue,CostQueue):
     costMin = 10000000000000000000000000000000000000000000
     idx=0
@@ -75,9 +92,14 @@ def popQueue(Queue,CostQueue):
     del CostQueue[idx]
     del Queue[idx]
     return TempPuzzle
+''' MAIN PROGRAM
+'''
 if(__name__== "__main__"):
+    # Inisiasi Puzzle Awal berupa matriks
     Puzzle=[[0 for i in range(4)] for i in range(4)]
+    # inisiasi nama file masukkan 
     namefile=input("Masukkan nama file: ")
+    # parsing masukkan file lalu masukkan kedalam Puzzle 
     with open(namefile) as f:
         lines = f.read().split("\n")
         content=[]
@@ -89,20 +111,32 @@ if(__name__== "__main__"):
                     Puzzle[i][j]=16
                 else:
                     Puzzle[i][j] = int(content[i][j])
+    # Print masukkan 15-Puzzle
     print("PUZZLE AWAL:")
     printPuzzle(Puzzle)
+    # Inisiasi level dari simpul untuk menghitung nilai f(x) pada cost(c(x))
     level=1
+    # Temporary Puzzle (digunakan sebagai pengganti solusi yang dipilih untuk setiap langkah)
     tempPuzzle=copy.deepcopy(Puzzle)
+    # Jumlah simpul yang dibangkitkan
     countSimpul=0
+    # Cek apakah Puzzle Solved 
     if(kurangPuzzle(Puzzle)):
         print("\nPATH MENUJU SOLUSI:")
+        # Inisiasi variabel untuk menghitung waktu yang diperlukan
         mulai = 0
+        # Variabel penampung Simpul-Simpul yang telah dibangkitkan
         history=[copy.deepcopy(Puzzle)]
+        # Variabel antrian Queue untuk simpul yang akan dibangkitkan
         queue=[copy.deepcopy(Puzzle)]
+        # Variabel penampul nilai Cost dari antrian Queue
         costQueue=[]
         mulai= mulai + time.time_ns()
+        # Jika g(x) bernilai nol hentikan pencarian solusi (urutan Puzzle sudah tepat)
         while(costPuzzle(tempPuzzle)!=0):
+            # Hapus elemen simpul yang akan dibangkitkan
             del queue[0]
+            # Puzzle yang dibangkitkan sesuai state yang mungkin
             upPuzzle=copy.deepcopy(tempPuzzle)
             downPuzzle=copy.deepcopy(tempPuzzle)
             leftPuzzle=copy.deepcopy(tempPuzzle)
@@ -110,40 +144,44 @@ if(__name__== "__main__"):
             for i in range(0,4):
                 for j in range(0,4):
                     if(tempPuzzle[i][j]==16):
-                        #cek down
+                        # Pengecekan state down
                         if(i-1>=0):
                             downPuzzle[i][j]=int(downPuzzle[i-1][j])
                             downPuzzle[i-1][j]=16
+                            # Pengecekkan apakah simpul yang dibangkitkan sudah pernah dibangkitkan sebelumnya
                             if(downPuzzle not in history):
                                 countSimpul = countSimpul + 1
                                 history.insert(0,(downPuzzle))
                                 queue.insert(0,(downPuzzle))
                                 cost=costPuzzle(downPuzzle)+level
                                 costQueue.insert(0,cost)
-                        #cek up
+                        # Pengecekan state up
                         if(i+1<4):
                             upPuzzle[i][j]=int(upPuzzle[i+1][j])
                             upPuzzle[i+1][j]=16
+                            # Pengecekkan apakah simpul yang dibangkitkan sudah pernah dibangkitkan sebelumnya
                             if(upPuzzle not in history):
                                 countSimpul = countSimpul + 1
                                 history.insert(0,(upPuzzle))
                                 queue.insert(0,(upPuzzle))
                                 cost=costPuzzle(upPuzzle)+level
                                 costQueue.insert(0,cost)
-                        #cek left
+                        # Pengecekan state left
                         if(j+1<4):
                             leftPuzzle[i][j]=int(leftPuzzle[i][j+1])
                             leftPuzzle[i][j+1]=16
+                            # Pengecekkan apakah simpul yang dibangkitkan sudah pernah dibangkitkan sebelumnya
                             if(leftPuzzle not in history):
                                 countSimpul = countSimpul + 1
                                 history.insert(0,(leftPuzzle))
                                 queue.insert(0,(leftPuzzle))
                                 cost=costPuzzle(leftPuzzle)+level
                                 costQueue.insert(0,cost)
-                        #cek right
+                        # Pengecekan state righta
                         if(j-1>=0):
                             rightPuzzle[i][j]=int(rightPuzzle[i][j-1])
                             rightPuzzle[i][j-1]=16
+                            # Pengecekkan apakah simpul yang dibangkitkan sudah pernah dibangkitkan sebelumnya
                             if(rightPuzzle not in history):
                                 countSimpul = countSimpul + 1
                                 history.insert(0,(rightPuzzle))
@@ -153,7 +191,9 @@ if(__name__== "__main__"):
                         break;
             level= level + 1
             tempPuzzle=copy.deepcopy(popQueue(queue,costQueue))
+            # Jika Antrian kosong hentikan pencarian solusi
             if(len(queue)==0):
+                print("Program dihentikan akibat antrian sudah kosong")
                 break;
             queue.insert(0,tempPuzzle)
             printPuzzle(tempPuzzle)
